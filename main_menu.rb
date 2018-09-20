a0682b69be302358fbfb1a4cd6e796eb68bdddcc
aritode <arian2k@gmail.com>
2018-09-20 04:01:25 +0300

    added MainMenu#take_seat_volume_carriage

--- main_menu.rb
+++ main_menu.rb
@@ -32,6 +32,7 @@ class MainMenu
         7. Remove Carriage from Train
         8. Move Train on the Route (Next/Previous)
         9. Show List of Stations and Trains on the Stations
+        10. Take Seat or Fill Volume in Carriage
         0. QUIT
     DISPLAY_MENU
     print '=> '
@@ -57,6 +58,8 @@ class MainMenu
       move_train_by_route
     when 9
       print_stations_and_trains
+    when 10
+      take_seat_volume_carriage
     else
       display_menu
     end
@@ -417,6 +420,54 @@ class MainMenu
         end
       else
         puts 'Empty'
+      end
+    end
+  end
+
+  def take_seat_volume_carriage
+    if @trains.empty?
+      puts 'You have no Trains. Please add Train and then Carriage'
+      add_carriage_to_train
+    else
+      title = 'Please choose Train:'
+      user_input = ordered_list_user_input(title, @trains)
+      user_train = @trains[user_input - 1]
+
+      if user_train.carriages.empty?
+        puts "\nThere is no Carriages in Train"
+        title = 'Do you want to create Carriage?'
+        custom_list = ['Yes, let\'s create Carriage',
+                       'No, let\'s go to Main Menu']
+        user_input = ordered_list_user_input(title, custom_list)
+        case user_input
+        when 1
+          add_carriage_to_train_core(user_train)
+        when 2
+          display_menu
+        end
+      else
+        title = 'Please choose Carriage:'
+        user_input = ordered_list_user_input(title, user_train.carriages)
+        user_carriage = user_train.carriages[user_input - 1]
+        puts user_carriage
+
+        if user_carriage.is_a? CargoCarriage
+          title = 'Please enter Volume to fill:'
+          user_input = characters_user_input(title).to_i
+          if user_carriage.take_volume(user_input)
+            puts "\n[SUCCESS] Volume #{user_input} is filled"
+          else
+            puts 'There is no available volume'
+          end
+        elsif user_carriage.is_a? PassengerCarriage
+          if user_carriage.take_seat
+            puts "\n[SUCCESS] Seat is taken"
+          else
+            puts 'There is no available seats'
+          end
+        else
+          puts 'ERROR'
+        end
       end
     end
   end
